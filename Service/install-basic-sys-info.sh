d4b29a843a463f095faf0bdc946ec70d1c3e42f4
Kuan-Yu Chang <kychang009@gmail.com>
2018-08-31 04:55:59 +0800

    clean code

--- Service/install-basic-sys-info.sh
+++ Service/install-basic-sys-info.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# useful veriable
 SERVICE=basic-sys-info
 
+# extends the sudo timeout for another 15 minutes
+sudo -v
+
+# install service
 sudo cp ${SERVICE}/${SERVICE}.sh /bin/${SERVICE}.sh
 sudo cp ${SERVICE}/${SERVICE} /etc/init.d/${SERVICE}
 sudo update-rc.d ${SERVICE} defaults
--- Service/install-hosts-monitor.sh
+++ Service/install-hosts-monitor.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# useful variable
+REPO_LINK="https://github.com/BassyKuo/hosts-monitor"
+
+# clone repo to /tmp
 cd /tmp
-git clone https://github.com/BassyKuo/hosts-monitor
+git clone "${REPO_LINK}"
+
+# extends the sudo timeout for another 15 minutes
+sudo -v
+
+# install service
 cd hosts-monitor
 sudo ./INSTALL
 sudo hosts-monitor service restart 
-cat /etc/ssh/sshd_banner 
+
+# clean up
+rm -rf /tmp/hosts-monitor
