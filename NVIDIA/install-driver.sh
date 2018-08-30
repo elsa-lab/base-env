a5ec73e4b713a65c866cf5896b41f79e5bc99e26
Kuan-Yu Chang <kychang009@gmail.com>
2018-08-30 14:42:42 +0800

    add installation script for NVIDIA driver

---
+++ NVIDIA/install-driver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# useful variables
+DRIVER_VERSION="390.67"
+DRIVER_INSTALLER="NVIDIA-Linux-x86_64-${DRIVER_VERSION}.run"
+DRIVER_LINK="http://us.download.nvidia.com/XFree86/Linux-x86_64/${DRIVER_VERSION}/${DRIVER_INSTALLER}"
+
+# install dkms first
+sudo -v
+sudo apt install -y dkms
+
+# download installer to /tmp
+cd /tmp
+curl -L "${DRIVER_LINK}" -O
+chmod +x "${DRIVER_INSTALLER}"
+
+# stop lightdm
+sudo service lightdm stop
+
+# install NVIDIA driver
+sudo ./"${DRIVER_INSTALLER}" \
+  --silent \
+  --dkms \
+  --no-opengl-files
+
+# enable persistence mode
+# you can also add this functionality to /etc/rc.local
+sudo nvidia-smi -pm 1
+
+# restart lightdm
+sudo service lightdm restart
+
+# clean up
+rm -f "${DRIVER_INSTALLER}"
