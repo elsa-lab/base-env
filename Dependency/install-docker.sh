93d752d621c1b1c716dd4b342caa4e93a4066d67
Kuan-Yu Chang <kychang009@gmail.com>
2018-08-30 11:30:49 +0800

    add default yes

--- Dependency/install-docker.sh
+++ Dependency/install-docker.sh
@@ -10,7 +10,7 @@ sudo sudo apt-get install -y \
   software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
   sudo apt-key add -
-sudo add-apt-repository \
+sudo add-apt-repository -y \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
