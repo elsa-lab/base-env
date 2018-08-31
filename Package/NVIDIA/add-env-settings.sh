eb9d8baed57563b5a9ec51b3144e119a02391864
Kuan-Yu Chang <kychang009@gmail.com>
2018-08-31 08:49:51 +0800

    enable persistence-mode on startup

--- Package/NVIDIA/add-env-settings.sh
+++ Package/NVIDIA/add-env-settings.sh
@@ -3,6 +3,7 @@
 # extends the sudo timeout for another 15 minutes
 sudo -v
 
+# some env variables for bash
 echo '
 ########## CUDA SECTION BEGIN ##########
 export PATH="/usr/local/cuda/bin:${PATH}"
@@ -11,3 +12,6 @@ export CUDA_HOME="/usr/local/cuda"
 export CUDA_VISIBLE_DEVICES="0"
 ########## CUDA SECTION END ##########
 ' | sudo tee --append /etc/bash.bashrc
+
+# enable persistence-mode on start up
+sudo sed -i -e '$i /usr/bin/nvidia-smi -pm 1\n' /etc/rc.local
