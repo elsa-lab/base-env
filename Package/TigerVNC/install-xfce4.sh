a95445583e2ffe1bfdd64cab7ec41dcc78f3655f
Kuan-Yu Chang <kychang009@gmail.com>
2018-09-04 00:21:27 +0800

    purge removed packages

--- Package/TigerVNC/install-xfce4.sh
+++ Package/TigerVNC/install-xfce4.sh
@@ -8,5 +8,10 @@ sudo apt-get update
 sudo apt-get install -y xfce4 xfce4-goodies
 
 # remove XScreenSaver, due to there is no real screen
-sudo apt-get remove -y xscreensaver
-sudo apt autoremove -y
+sudo apt-get purge -y xscreensaver
+
+# remove xdg-user-dirs, since we dont need those default dirs
+sudo apt-get purge -y xdg-user-dirs
+
+# clean up
+sudo apt autoremove -y --purge
