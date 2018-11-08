b0e784bf4e4a2473a6b63670d33922d4641d48f8
Kuan-Yu Chang <kychang009@gmail.com>
2018-11-08 09:23:07 +0800

    change wget to curl

--- Package/Dependency/install-snoopy.sh
+++ Package/Dependency/install-snoopy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# extends the sudo timeout for another 15 minutes
-sudo -v
-
 # download installation script to /tmp
 cd /tmp
-wget -O snoopy-install.sh https://github.com/a2o/snoopy/raw/install/doc/install/bin/snoopy-install.sh
+curl -LO https://github.com/a2o/snoopy/raw/install/doc/install/bin/snoopy-install.sh
 chmod +x snoopy-install.sh
+
+# extends the sudo timeout for another 15 minutes
+sudo -v
 
 # install snoopy
 sudo ./snoopy-install.sh stable
