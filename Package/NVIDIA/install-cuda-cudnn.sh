30cf49771abccb1f105a0a7b3376596c8447fb68
Kuan-Yu Chang <kychang009@gmail.com>
2019-03-28 14:08:50 +0800

    add cuda10

--- Package/NVIDIA/install-cuda-cudnn.sh
+++ Package/NVIDIA/install-cuda-cudnn.sh
@@ -2,20 +2,26 @@
 
 # useful variables
 CUDA8_LINK="https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run"
-CUDA9_LINK="https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run"
+CUDA8_INSTALLER="cuda_8.0.61_375.26_linux.run"
 
 CUDA8_CUDNN5_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
 CUDA8_CUDNN6_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz"
 CUDA8_CUDNN7_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v7.1.2/cudnn-8.0-linux-x64-v7.1.tgz"
-CUDA9_CUDNN7_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-9.0-linux-x64-v7.3.1.20.tgz"
-
-CUDA8_INSTALLER="cuda_8.0.61_375.26_linux.run"
-CUDA9_INSTALLER="cuda_9.0.176_384.81_linux.run"
-
 CUDA8_CUDNN5_TGZ="cudnn-8.0-linux-x64-v5.1.tgz"
 CUDA8_CUDNN6_TGZ="cudnn-8.0-linux-x64-v6.0.tgz"
 CUDA8_CUDNN7_TGZ="cudnn-8.0-linux-x64-v7.1.tgz"
+
+CUDA9_LINK="https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run"
+CUDA9_INSTALLER="cuda_9.0.176_384.81_linux.run"
+
+CUDA9_CUDNN7_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.1/cudnn-9.0-linux-x64-v7.3.1.20.tgz"
 CUDA9_CUDNN7_TGZ="cudnn-9.0-linux-x64-v7.3.1.20.tgz"
+
+CUDA10_LINK="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
+CUDA10_INSTALLER="cuda_10.0.130_410.48_linux.run"
+
+CUDA10_CUDNN7_LINK="http://developer.download.nvidia.com/compute/redist/cudnn/v7.5.0/cudnn-10.0-linux-x64-v7.5.0.56.tgz"
+CUDA10_CUDNN7_TGZ="cudnn-10.0-linux-x64-v7.5.0.56.tgz"
 
 TEMP_PATH="/tmp/cuda-$(date | md5sum | awk '{print $1}')"
 
@@ -26,14 +32,17 @@ cd "${TEMP_PATH}"
 # download cuda installer
 curl -L "${CUDA8_LINK}" -o "${CUDA8_INSTALLER}"
 curl -L "${CUDA9_LINK}" -o "${CUDA9_INSTALLER}"
+curl -L "${CUDA10_LINK}" -o "${CUDA10_INSTALLER}"
 chmod +x "${CUDA8_INSTALLER}"
 chmod +x "${CUDA9_INSTALLER}"
+chmod +x "${CUDA10_INSTALLER}"
 
 # download cudnn tgz
 curl -LO "${CUDA8_CUDNN5_LINK}"
 curl -LO "${CUDA8_CUDNN6_LINK}"
 curl -LO "${CUDA8_CUDNN7_LINK}"
 curl -LO "${CUDA9_CUDNN7_LINK}"
+curl -LO "${CUDA10_CUDNN7_LINK}"
 
 # extends the sudo timeout for another 15 minutes
 sudo -v
@@ -52,6 +61,12 @@ sudo ./"${CUDA9_INSTALLER}" --silent --toolkit --verbose
 # install cudnn library to cuda9 home
 sudo tar --no-same-owner -xzf "${CUDA9_CUDNN7_TGZ}" -C /usr/local --wildcards 'cuda/lib64/libcudnn.so.*'
 
+# install cuda10 toolkit
+sudo ./"${CUDA10_INSTALLER}" --silent --toolkit --verbose
+
+# install cudnn library to cuda10 home
+sudo tar --no-same-owner -xzf "${CUDA10_CUDNN7_TGZ}" -C /usr/local --wildcards 'cuda/lib64/libcudnn.so.*'
+
 # remove default symbloic link
 sudo rm -f /usr/local/cuda
 
@@ -65,8 +80,8 @@ export CUDA_VISIBLE_DEVICES="0"
 ' | sudo tee /etc/profile.d/cuda.sh
 
 # default use cuda-9.0
-ln -n -f -s /usr/local/cuda-9.0 ~/.cuda
-sudo ln -n -f -s /usr/local/cuda-9.0 /etc/skel/.cuda
+ln -n -f -s /usr/local/cuda-10.0 ~/.cuda
+sudo ln -n -f -s /usr/local/cuda-10.0 /etc/skel/.cuda
 
 # clean up
 cd && rm -rf "${TEMP_PATH}"
