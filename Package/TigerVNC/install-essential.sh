d8fd421369b245dd016c6b875f2ab5a83aec670d
Yu <kychang009@gmail.com>
2019-07-25 08:18:26 +0800

    Update install-essential.sh

--- Package/TigerVNC/install-essential.sh
+++ Package/TigerVNC/install-essential.sh
@@ -5,10 +5,13 @@ sudo -v
 
 # install web browser
 sudo apt update
-sudo apt install -y chromium-browser
-sudo apt install -y fonts-moe-standard-kai
-sudo apt install -y fonts-moe-standard-song
+sudo apt install -y chromium-browser \
+                    fonts-moe-standard-kai \
+                    fonts-moe-standard-song
 
 # install visual studio code
 curl -L https://vscode.cdn.azure.cn/stable/2213894ea0415ee8c85c5eea0d0ff81ecc191529/code_1.36.1-1562627527_amd64.deb -o /tmp/vs-code.deb
 sudo dpkg -i /tmp/vs-code.deb
+
+# clean up
+rm -f /tmp/vs-code.deb
