cbe1e5ebefb3f0515e7e4c53b57c12bb6d97e2e9
Kuan-Yu Chang <kychang009@gmail.com>
2020-03-29 22:57:25 +0800

    modify the packages for VNC

--- Package/TigerVNC/install-essential.sh
+++ Package/TigerVNC/install-essential.sh
@@ -5,12 +5,10 @@ sudo -v
 
 # install web browser
 sudo apt update
-sudo apt install -y chromium-browser \
-                    fonts-moe-standard-kai \
-                    fonts-moe-standard-song
+sudo apt install -y firefox fonts-moe-standard-kai
 
 # install visual studio code
-curl -L https://vscode.cdn.azure.cn/stable/2213894ea0415ee8c85c5eea0d0ff81ecc191529/code_1.36.1-1562627527_amd64.deb -o /tmp/vs-code.deb
+curl -L 'https://go.microsoft.com/fwlink/?LinkID=760868' -o /tmp/vs-code.deb
 sudo dpkg -i /tmp/vs-code.deb
 
 # clean up
