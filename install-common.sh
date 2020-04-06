1ba7f2bbe0906cf6cd7508b59882c8288494aa70
Kuan-Yu Chang <kychang009@gmail.com>
2020-04-06 23:55:04 +0800

    add to install common

--- install-common.sh
+++ install-common.sh
@@ -43,6 +43,7 @@ cd ${WORKING_DIR}/Package
 ## Package: NVIDIA
 ./NVIDIA/install-nv-driver.sh
 ./NVIDIA/install-cuda-cudnn.sh
+./NVIDIA/install-nv-docker.sh
 
 ## Package: TigerVNC
 ./TigerVNC/install-xfce4.sh
