d8a2d5d7464265ff8ac344313106bd2b9e83c3c3
root <Yu>
2020-05-01 09:22:58 +0800

    modify installation process of docker

--- Package/Dependency/install-docker.sh
+++ Package/Dependency/install-docker.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
 # extends the sudo timeout for another 15 minutes
 sudo -v
 
-# uninstall old versions, it’s OK if apt-get reports that none of these packages are installed
-sudo apt-get purge -y docker docker-engine docker.io
+# update apt repo
+sudo apt update
 
-# add the package repositories
-sudo apt-get update
-sudo apt-get install -y \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo apt-key add -
-sudo add-apt-repository -y \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# install docker
+sudo apt install -y docker.io
 
-# install docker CE
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# start docker
+sudo systemctl enable --now docker
 
-# install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) \
-  -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# add current user to docker group
+sudo usermod -aG docker ${USER}
+
--- Package/Dependency/install-essential.sh
+++ Package/Dependency/install-essential.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Cause the script to exit on any errors
 # Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
