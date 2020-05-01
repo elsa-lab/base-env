47a9f9e836612bb1f00bac8c405f5434edd94a3c
root <Yu>
2020-05-01 09:31:04 +0800

    change the installation process of mujoco, since ppa:jamesh/snap-support is invalid in Ubuntu 20.04

--- Package/Dependency/install-mujoco.sh
+++ Package/Dependency/install-mujoco.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
 # useful variables
 MJPRO_VERSIONS="131 140 150"
 MJ_VERSIONS="200"
@@ -15,16 +19,16 @@ mkdir -p "${TEMP_PATH}"
 cd "${TEMP_PATH}"
 
 # download getid
-curl -LO "https://www.roboti.us/getid/getid_linux"
+curl -sSLO "https://www.roboti.us/getid/getid_linux"
 chmod +x getid_linux
 
 # download zipfiles of mujoco
 for v in ${MJPRO_VERSIONS}; do
-  curl -LO "${LINK_PREFIX}/mjpro${v}${LINK_POSTFIX}"
+  curl -sSLO "${LINK_PREFIX}/mjpro${v}${LINK_POSTFIX}"
 done
 
 for v in ${MJ_VERSIONS}; do
-  curl -LO "${LINK_PREFIX}/mujoco${v}${LINK_POSTFIX}"
+  curl -sSLO "${LINK_PREFIX}/mujoco${v}${LINK_POSTFIX}"
 done
 
 # unzip mujoco then clean the zipfile
@@ -34,7 +38,7 @@ for z in *.zip; do
 done
 
 # rename mujoco200
-ln -s mujoco200_linux mujoco200
+ln -fsn mujoco200_linux mujoco200
 
 # extends the sudo timeout for another 15 minutes
 sudo -v
@@ -44,13 +48,13 @@ sudo mkdir -p "${MJ_PATH}"
 sudo cp -r * "${MJ_PATH}"
 
 # make symbolic link to home
-ln -n -f -s "${MJ_PATH}" "${HOME}/.mujoco"
-sudo ln -n -f -s "${MJ_PATH}" "/etc/skel/.mujoco"
+ln -fsn "${MJ_PATH}" "${HOME}/.mujoco"
+sudo ln -fsn "${MJ_PATH}" "/etc/skel/.mujoco"
 
 # clean up
 cd && rm -rf "${TEMP_PATH}"
 
 # install PatchELF (for mujoco-py)
-sudo add-apt-repository -y ppa:jamesh/snap-support
-sudo apt-get update
-sudo apt-get install -y patchelf
+# sudo add-apt-repository -y ppa:jamesh/snap-support
+# sudo apt-get update
+# sudo apt-get install -y patchelf
