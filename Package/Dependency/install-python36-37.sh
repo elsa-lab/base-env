74e78188b7a48339c924fcdaa55ee499e80b901e
root <kychang009@gmail.com>
2020-05-01 09:43:22 +0800

    modify script

--- Package/Dependency/install-python36-37.sh
+++ Package/Dependency/install-python36-37.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
 # extends the sudo timeout for another 15 minutes
 sudo -v
 
