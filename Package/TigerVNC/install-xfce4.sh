0c47714fc252bf32a484cd252df367226889718b
Yu <kychang009@gmail.com>
2020-05-03 23:31:57 +0800

    Update install-xfce4.sh

--- Package/TigerVNC/install-xfce4.sh
+++ Package/TigerVNC/install-xfce4.sh
@@ -7,15 +7,18 @@ set -euo pipefail
 # extends the sudo timeout for another 15 minutes
 sudo -v
 
+# purge display manager
+sudo apt purge -y gdm3 lightdm
+
 # install Xfce desktop environment
-sudo apt-get update
-sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt update
+sudo apt install -y xfce4 xfce4-goodies
 
 # remove XScreenSaver, due to there is no real screen
-sudo apt-get purge -y xscreensaver
+sudo apt purge -y xscreensaver
 
 # remove xdg-user-dirs, since we dont need those default dirs
-sudo apt-get purge -y xdg-user-dirs
+sudo apt purge -y xdg-user-dirs
 
 # clean up
-sudo apt-get autoremove -y --purge
+sudo apt autoremove -y --purge
