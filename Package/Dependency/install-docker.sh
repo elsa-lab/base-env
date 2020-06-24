a99831f1b968caffcf4f9a5a30e255b45297dd44
Yu <kychang009@gmail.com>
2020-06-24 18:19:45 +0800

    add docker-compose

--- Package/Dependency/install-docker.sh
+++ Package/Dependency/install-docker.sh
@@ -19,3 +19,6 @@ sudo systemctl enable --now docker
 # add current user to docker group
 sudo usermod -aG docker ${USER}
 
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
