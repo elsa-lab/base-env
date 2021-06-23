14f1f1b26d62f13c9583ae232de443f47b93831a
Kuan-Yu Chang <kychang009@gmail.com>
2021-06-23 19:32:51 +0800

    add VirtualGL installation script

---
+++ Package/Dependency/install-virtualgl.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+# download VirtualGL
+curl -sSL https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download -o /tmp/virtualgl.deb
+
+# create an appropriate xorg.conf file for headless operation
+sudo nvidia-xconfig -a --allow-empty-initial-configuration --virtual=1920x1200
+
+# install display manager
+sudo apt install lightdm -y
+
+# install VirtualGL
+sudo dpkg -i /tmp/virtualgl.deb
+
+# configure VirtualGL
+sudo vglserver_config -config
+
+# add users for accessing vglserver
+sudo usermod -aG root vglusers
+
+# restart display manager
+sudo service lightdm restart
+
+echo -e "\e[5m\e[1mReboot required.\e[0m"
