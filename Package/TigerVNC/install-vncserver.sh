6c6e93e7b976f517b481dd1d2648286e500205a7
Kuan-Yu <kychang009@gmail.com>
2021-06-28 16:42:16 +0800

    remove vs-code

--- Package/TigerVNC/install-vncserver.sh
+++ Package/TigerVNC/install-vncserver.sh
@@ -15,10 +15,3 @@ sudo apt install -y tigervnc-standalone-server tigervnc-xorg-extension
 
 # install web browser
 sudo apt install -y firefox fonts-moe-standard-kai
-
-# install visual studio code
-curl -sSL 'https://go.microsoft.com/fwlink/?LinkID=760868' -o /tmp/vs-code.deb
-sudo dpkg -i /tmp/vs-code.deb
-
-# clean up
-rm -f /tmp/vs-code.deb
