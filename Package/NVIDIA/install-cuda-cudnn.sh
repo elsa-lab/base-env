6bd3343990e53aba4a323ae63edd423711c70d19
sky9023 <25349023.qq@gmail.com>
2023-05-31 22:38:34 +0800

    fix driver version; minor fixes

--- Package/NVIDIA/install-cuda-cudnn.sh
+++ Package/NVIDIA/install-cuda-cudnn.sh
@@ -52,6 +52,7 @@ install () {
       sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
       sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64
       sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+      sudo rm -r cudnn-*-archive
   fi
   echo "Done."
 
@@ -60,8 +61,6 @@ install () {
 
   # clean up
   rm cuda.run "${CUDNN_FILE}"
-  
-  ls cudnn-*-archive && sudo rm -r cudnn-*-archive || :
 }
 
 # install each version of CUDA and cuDNN
