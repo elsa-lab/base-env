c25ddef5ff0c5088cdf6033a689ba4b042d63150
25349023 <25349023.qq@gmail.com>
2024-04-07 17:27:56 +0800

    improved flow for downloading cudnn installer from elsa server

--- Package/NVIDIA/install-cuda-cudnn.sh
+++ Package/NVIDIA/install-cuda-cudnn.sh
@@ -34,7 +34,7 @@ install () {
   then
       curl -sSL "${CUDNN_LINK}" -o "${CUDNN_FILE}"
   else
-      scp "$(logname)@elsa:/data_10T/install_archive/${CUDNN_LINK}" "${CUDNN_FILE}"
+      scp -i ~/.ssh/elsa-server "$(logname)@elsa.cs.nthu.edu.tw:/data_10T/install_archive/${CUDNN_LINK}" "${CUDNN_FILE}"
   fi
       
   echo "Done."
@@ -80,9 +80,10 @@ install () {
 # install "10.1" "7.6.0" \
 #   "http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run" \
 #   "http://developer.download.nvidia.com/compute/redist/cudnn/v7.6.0/cudnn-10.1-linux-x64-v7.6.0.64.tgz"
-# install "10.2" "7.6.5" \
-#   "http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run" \
-#   "http://developer.download.nvidia.com/compute/redist/cudnn/v7.6.5/cudnn-10.2-linux-x64-v7.6.5.32.tgz"
+install "10.2" "7.6.5" \
+  "http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run" \
+  "http://developer.download.nvidia.com/compute/redist/cudnn/v7.6.5/cudnn-10.2-linux-x64-v7.6.5.32.tgz"
+
 # install "11.0" "8.0.4" \
 #   "http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run" \
 #   "http://developer.download.nvidia.com/compute/redist/cudnn/v8.0.4/cudnn-11.0-linux-x64-v8.0.4.30.tgz"
@@ -92,19 +93,19 @@ install () {
 # install "11.2.2" "8.1.1" \
 #   "https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run" \
 #   "http://developer.download.nvidia.com/compute/redist/cudnn/v8.1.1/cudnn-11.2-linux-x64-v8.1.1.33.tgz"
-# 
-# install "11.3" "8.6.0" \
-#   "https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run" \
-#   "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
-# install "11.6" "8.6.0" \
-#   "https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run" \
-#   "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
+ 
+install "11.3" "8.6.0" \
+  "https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run" \
+  "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
+install "11.6" "8.6.0" \
+  "https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run" \
+  "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
 # install "11.7" "8.6.0" \
 #   "https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/cuda_11.7.1_515.65.01_linux.run" \
 #   "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
-# install "11.8" "8.6.0" \
-#   "https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run" \
-#   "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
+install "11.8" "8.6.0" \
+  "https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run" \
+  "https://developer.download.nvidia.com/compute/redist/cudnn/v8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz"
 
 install "12.0" "8.9.6" \
   "https://developer.download.nvidia.com/compute/cuda/12.0.1/local_installers/cuda_12.0.1_525.85.12_linux.run" \
@@ -116,17 +117,7 @@ install "12.2" "8.9.6" \
   "https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda_12.2.1_535.86.10_linux.run" \
   "cudnn-linux-x86_64-8.9.6.50_cuda12-archive.tar.xz"
 
-# default use cuda-11.7
-ln -fns /usr/local/cuda-11.7 ~/.cuda
-sudo ln -fns /usr/local/cuda-11.7 /etc/skel/.cuda
-
-
-# default use cuda-11.7
-ln -fns /usr/local/cuda-11.7 ~/.cuda
-sudo ln -fns /usr/local/cuda-11.7 /etc/skel/.cuda
-
-
-# default use cuda-11.7
-ln -fns /usr/local/cuda-11.7 ~/.cuda
-sudo ln -fns /usr/local/cuda-11.7 /etc/skel/.cuda
+# default use cuda-11.6
+ln -fns /usr/local/cuda-11.6 ~/.cuda
+sudo ln -fns /usr/local/cuda-11.6 /etc/skel/.cuda
 
