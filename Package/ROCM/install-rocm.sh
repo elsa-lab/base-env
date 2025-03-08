110b842ddcbf931cffc1c82777059a2fb1a9a955
AlanHuang <alan0988469026@gmail.com>
2025-03-08 15:27:37 +0800

    Fix typo

--- Package/ROCM/install-rocm.sh
+++ Package/ROCM/install-rocm.sh
@@ -12,7 +12,7 @@ UBUNTU_CODENAME=$(lsb_release -cs)
 ROCM_VERS="6.3.3 6.2.4"
 
 # Add the ROCm repositories
-printf" Registering ROCm repositories ..."
+printf "Registering ROCm repositories ..."
 for $ROCM_VER in $ROCM_VERS; do
   echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/${ROCM_VER} ${UBUNTU_CODENAME} main" \
       | sudo tee --append /etc/apt/sources.list.d/rocm.list
@@ -22,13 +22,13 @@ echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
 echo "Done."
 
 # Install ROCm
-printf" Installing ROCm ..."
+printf "Installing ROCm ..."
 sudo apt update
 for $ROCM_VER in $ROCM_VERS; do
   # extends the sudo timeout for another minutes
   sudo -v 
 
-  printf" Installing ROCm ${ROCM_VER} ..."
+  printf "Installing ROCm ${ROCM_VER} ..."
   sudo amdgpu-install -y --usecase=rocm --rocmrelease=${ROCM_VER}
   echo "ROCm ${ROCM_VER} installed."
 done
